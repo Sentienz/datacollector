60959da8a3012e2ef1b5b0658f3e5bb16ffc356d
rifat-khanam <52399179+rifat-khanam@users.noreply.github.com>
2019-07-23 14:18:58 +0530

    Update SASDataParser.java

--- commonlib/src/main/java/com/streamsets/pipeline/lib/parser/sas/SASDataParser.java
+++ commonlib/src/main/java/com/streamsets/pipeline/lib/parser/sas/SASDataParser.java
@@ -1,6 +1,9 @@
 package com.streamsets.pipeline.lib.parser.sas;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -23,11 +26,12 @@ public class SASDataParser extends AbstractDataParser {
 	private static final String OFFSET_MINUS_ONE = "-1";
 	private static final String OFFSET_ZERO = "0";
 	private boolean isClosed;
-	private boolean alreadyParsed;
+	private boolean alreadyParsed = false;
 	private String id;
 	private String offset;
 	private long recordCount;
 	private boolean eof;
+	int currentOffset;
 
 	public SASDataParser(SasFileReader sasFileReader, ProtoConfigurableEntity.Context context, String id,
 			String offset) {
@@ -37,60 +41,71 @@ public class SASDataParser extends AbstractDataParser {
 		this.offset = offset;
 		this.sasFileProperties = sasFileReader.getSasFileProperties();
 		this.recordCount = sasFileProperties.getRowCount();
-	}
-
+		}																																																																																	
+	
 	@Override
 	public Record parse() throws IOException, DataParserException {
 		Record record = null;
 		if (isClosed) {
 			throw new IOException("The parser is closed");
 		}
+		if(!alreadyParsed) {
 		record = updateRecordsWithHeader(record);
 		alreadyParsed = true;
-		if(record!=null) {
-		  recordCount++;
+		}
+		else {
+		eof=true;
 		}
 		return record;
-	}
-
-	@Override
-	public String getOffset(){
-	  return eof ? String.valueOf(-1) : sasFileReader.getOffset().toString();
 	}
 
 	@Override
 	public void close() throws IOException {
 		isClosed = true;
 	}
-
-	private Record updateRecordsWithHeader(Record record) throws IOException {
-	    
-	    Object[] rows = sasFileReader.readNext();
-	    if(rows==null || rows.length==0) {
-	      eof=true;
-	      return null;
-	    }
-	  
-	    record = context.createRecord(id + "::" + recordCount);
-	  
-		List<Column> columnList = sasFileReader.getColumns();
-		headers = new ArrayList<>();
-		for (Column col : sasFileReader.getColumns()) {
-			headers.add(Field.create(col.getName()));
-		}
-		LinkedHashMap<String, Field> listMap = new LinkedHashMap<>();
-		for (int i = 0; i < columnList.size(); i++) {
-			String key;
-			Field header = (headers != null) ? headers.get(i) : null;
-			if (header != null) {
-				key = header.getValueAsString();
-			} else {
-				key = Integer.toString(i);
-			}
-			listMap.put(key, Field.create(Field.Type.STRING, rows[i]));
-		}
-		record.set(Field.createListMap(listMap));
-		return record;
+	
+	@Override
+	public String getOffset() throws  IOException {
+		  return eof ? String.valueOf(-1) : sasFileReader.getOffset().toString();
 	}
 
+	
+	private Record updateRecordsWithHeader(Record record) throws IOException {
+		currentOffset = Integer.valueOf(sasFileReader.getOffset());	
+		if(Integer.parseInt(offset)!=currentOffset) {
+			currentOffset = getCurrentOffset(Integer.parseInt(offset),currentOffset);
+		}
+		record = context.createRecord(id + "::" + currentOffset);
+		Object rows[] = sasFileReader.readNext();
+		List<Column> columnlist = sasFileReader.getColumns();
+		headers = new ArrayList<Field>();
+		for(Column col :sasFileReader.getColumns()) {
+			headers.add(Field.create(col.getName()));
+		}		
+		
+		LinkedHashMap<String,Field> listMap = new LinkedHashMap<String,Field>();
+		for(int i = 0; i<columnlist.size();i++) {
+			String key;
+			Field header = (headers!=null)?headers.get(i):null;
+			if(headers!=null) {
+				key=header.getValueAsString();
+			}
+			else {
+				key = Integer.toString(i);
+			}
+			listMap.put(key,Field.create(Field.Type.STRING,rows[i]));	
+			}
+		
+		record.set(Field.createListMap(listMap));
+		
+	return record;
+	}
+
+	private int getCurrentOffset(int offset, int currentOffset) throws IOException {
+		while(offset!=currentOffset){
+			Object rows[] = sasFileReader.readNext();
+			currentOffset = sasFileReader.getOffset();
+		}
+		return sasFileReader.getOffset();
+	}
 }
