d38437c6ddd46a4ecd5e78b9c76840c11f1ac365
vava02 <chitra.nair@sentienz.com>
2019-07-30 12:22:08 +0530

    Added SAS Test classes

--- commonlib/src/test/java/com/streamsets/pipeline/lib/parser/sas/TestSASDataParser.java
+++ commonlib/src/test/java/com/streamsets/pipeline/lib/parser/sas/TestSASDataParser.java
@@ -1,11 +1,17 @@
 package com.streamsets.pipeline.lib.parser.sas;
 
+import com.epam.parso.SasFileProperties;
 import com.epam.parso.SasFileReader;
 import com.epam.parso.impl.SasFileReaderImpl;
+import com.sentienz.sas.xpt.SASXportFileIterator;
 import com.streamsets.pipeline.api.OnRecordError;
 import com.streamsets.pipeline.api.Record;
 import com.streamsets.pipeline.api.Stage;
+import com.streamsets.pipeline.api.ext.json.Mode;
 import com.streamsets.pipeline.lib.parser.DataParser;
+import com.streamsets.pipeline.lib.parser.DataParserException;
+import com.streamsets.pipeline.lib.parser.json.JsonCharDataParser;
+import com.streamsets.pipeline.lib.parser.sasxpt.SASXPTDataParser;
 import com.streamsets.pipeline.sdk.ContextInfoCreator;
 
 import java.io.FileNotFoundException;
@@ -20,64 +26,83 @@ import org.junit.Test;
 
 public class TestSASDataParser {
 
-    private InputStream is = new FileInputStream("/Users/chitranair/Downloads/samp.sas7bdat");
+
+	InputStream is = getClass().getClassLoader().getResourceAsStream("samp1.sas7bdat");
+
 
     public TestSASDataParser() throws FileNotFoundException {
     }
 
     private Stage.Context getContext() {
-        return ContextInfoCreator.createSourceContext("i", false, OnRecordError.TO_ERROR, Collections.emptyList());
+        return ContextInfoCreator.createSourceContext("i", true, OnRecordError.TO_ERROR, Collections.emptyList());
     }
 
-
     @Test
-    public void TestSASWithoutOffset() throws Exception {
+    public void TestSAS() throws Exception {
 
         SasFileReader sasFileReader = new SasFileReaderImpl(is);
         DataParser parser = new SASDataParser(sasFileReader , getContext(),"id" , "0");
-
         Assert.assertEquals(0, Long.parseLong(parser.getOffset()));
         Record record = parser.parse();
         System.out.print(record);
-        Assert.assertEquals(1, Long.parseLong(parser.getOffset()));
-        Assert.assertEquals("id::0", record.getHeader().getSourceId());
-        Assert.assertEquals("BILI", record.get().getValueAsList().get(7).getValueAsString());
+        Assert.assertNotNull(record);    
+        Assert.assertEquals("id::0", record.getHeader().getSourceId());   
+        Assert.assertEquals("AAAA", record.get().getValueAsList().get(5).getValueAsString());
+        Assert.assertEquals(1,Long.parseLong(parser.getOffset())); 
         record = parser.parse();
-        Assert.assertNull(record);
-        Assert.assertEquals(-1, Long.parseLong(parser.getOffset()));
-        parser.close();
-
+        Assert.assertNotNull(record);
     }
 
-
+    
     @Test
-    public void TestSASWithOffset() throws Exception {
+    public void TestSASwithOffset() throws Exception {
 
         SasFileReader sasFileReader = new SasFileReaderImpl(is);
-        DataParser parser = new SASDataParser(sasFileReader , getContext(),"id" , "99600");
-
-        Assert.assertEquals(0, Long.parseLong(parser.getOffset()));
-        System.out.println(parser.getOffset());
+        DataParser parser = new SASDataParser(sasFileReader , getContext(),"id" , "5");
+        Assert.assertEquals(5, Long.parseLong(parser.getOffset()));
         Record record = parser.parse();
         System.out.print(record);
-        Assert.assertEquals(99601, Long.parseLong(parser.getOffset()));
-        Assert.assertEquals("id::99600", record.getHeader().getSourceId());
-        Assert.assertEquals("AST", record.get().getValueAsList().get(7).getValueAsString());
+        Assert.assertNotNull(record);
+        Assert.assertEquals("ffffffff", record.get().getValueAsList().get(0).getValueAsString());   
+        Assert.assertEquals("id::5", record.getHeader().getSourceId());       
+        Assert.assertEquals(6,Long.parseLong(parser.getOffset()));  
         record = parser.parse();
-        System.out.print(record);
-        Assert.assertNull(record);
-        Assert.assertEquals(-1, Long.parseLong(parser.getOffset()));
-        parser.close();
+        Assert.assertEquals(7, Long.parseLong(parser.getOffset()));
+     
+    }
+    @Test
+    public void TestSASLastOffset() throws Exception {
 
+        SasFileReader sasFileReader = new SasFileReaderImpl(is);
+        DataParser parser = new SASDataParser(sasFileReader , getContext(),"id" , "49");
+        Assert.assertEquals(49, Long.parseLong(parser.getOffset()));
+        Record record = parser.parse();
+        System.out.print(record);
+        Assert.assertNotNull(record);
+        Assert.assertEquals("zzzzzzzz", record.get().getValueAsList().get(0).getValueAsString());   
+        Assert.assertEquals("id::49", record.getHeader().getSourceId());       
+        Assert.assertEquals(50,Long.parseLong(parser.getOffset()));  
+        record = parser.parse();
+        Assert.assertEquals(-1, Long.parseLong(parser.getOffset()));
+        record = parser.parse();
+        Assert.assertNull(record);
     }
 
+    
+    @Test
+    public void testgetOffset() throws NumberFormatException, DataParserException, IOException {
+    	SasFileReader sasFileReader = new SasFileReaderImpl(is); ;
+        DataParser parser = new SASDataParser(sasFileReader , getContext(),"0" , "43");
+        Assert.assertEquals(43, Long.parseLong(parser.getOffset()));  
+    }
+    
     @Test(expected = IOException.class)
     public void testClose() throws Exception {
-        SasFileReader sasFileReader = new SasFileReaderImpl(is);
+        SasFileReader sasFileReader = new SasFileReaderImpl(is); ;
         DataParser parser = new SASDataParser(sasFileReader , getContext(),"0" , "0");
 
         parser.close();
         parser.parse();
     }
 
-}
+} 
--- commonlib/src/test/java/com/streamsets/pipeline/lib/parser/sasxpt/TestSASXPTDataParser.java
+++ commonlib/src/test/java/com/streamsets/pipeline/lib/parser/sasxpt/TestSASXPTDataParser.java
@@ -1,41 +1,52 @@
 package com.streamsets.pipeline.lib.parser.sasxpt;
 
+import com.epam.parso.SasFileReader;
+import com.epam.parso.impl.SasFileReaderImpl;
 import com.sentienz.sas.xpt.SASXportFileIterator;
 import com.streamsets.pipeline.api.OnRecordError;
 import com.streamsets.pipeline.api.Record;
 import com.streamsets.pipeline.api.Stage;
+import com.streamsets.pipeline.api.ext.io.OverrunReader;
+import com.streamsets.pipeline.api.ext.json.Mode;
 import com.streamsets.pipeline.lib.parser.DataParser;
+import com.streamsets.pipeline.lib.parser.DataParserException;
+import com.streamsets.pipeline.lib.parser.json.JsonCharDataParser;
+import com.streamsets.pipeline.lib.parser.sas.SASDataParser;
 import com.streamsets.pipeline.sdk.ContextInfoCreator;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
 import java.util.Collections;
 
 
 public class TestSASXPTDataParser {
+
+    InputStream inputStream = getClass()
+            .getClassLoader().getResourceAsStream("test.xpt");
 
     private Stage.Context getContext() {
         return ContextInfoCreator.createSourceContext("i", false, OnRecordError.TO_ERROR, Collections.emptyList());
     }
 
     @Test
-    public void TestSASXPTWithoutOffset() throws Exception {
+    public void TestSASXPT() throws Exception {
 
-        SASXportFileIterator iterator = new SASXportFileIterator("/Users/chitranair/Downloads/data/test.xpt",0);
+        SASXportFileIterator iterator = new SASXportFileIterator(inputStream);
         DataParser parser = new SASXPTDataParser(iterator, getContext(), "id","0");
-
-        Assert.assertEquals(1312, Long.parseLong(parser.getOffset()));
+        Assert.assertEquals(1, Long.parseLong(parser.getOffset()));
         Record record = parser.parse();
         System.out.print(record);
         Assert.assertNotNull(record);
         System.out.print(record);
-        Assert.assertEquals("id::0", record.getHeader().getSourceId());
-        Assert.assertEquals("Name", record.get().getValueAsList().get(0).getValueAsString());
-        Assert.assertEquals(10, Long.parseLong(parser.getOffset()));
+        Assert.assertEquals("id::1", record.getHeader().getSourceId());
+        Assert.assertEquals("Allanson, Andy", record.get().getValueAsList().get(0).getValueAsString());
+        Assert.assertEquals(2, Long.parseLong(parser.getOffset()));
         record = parser.parse();
-        Assert.assertNull(record);
-        Assert.assertEquals(-1, Long.parseLong(parser.getOffset()));
+        Assert.assertNotNull(record);
+        Assert.assertEquals(3, Long.parseLong(parser.getOffset()));
         parser.close();
 
     }
@@ -44,33 +55,62 @@ public class TestSASXPTDataParser {
     @Test
     public void TestSASXPTWithOffset() throws Exception {
 
-        SASXportFileIterator iterator = new SASXportFileIterator("/Users/chitranair/Downloads/data/test.xpt",0);
-        DataParser parser = new SASXPTDataParser(iterator, getContext(), "id","0");
-
-        Assert.assertEquals(1312, Long.parseLong(parser.getOffset()));
+        SASXportFileIterator iterator = new SASXportFileIterator(inputStream);
+        DataParser parser = new SASXPTDataParser(iterator, getContext(), "id","7");
+        Assert.assertEquals(7,Long.parseLong(parser.getOffset()));
         Record record = parser.parse();
-        System.out.print(record);
         Assert.assertNotNull(record);
-        System.out.print(record);
-        Assert.assertEquals("id::0", record.getHeader().getSourceId());
-        Assert.assertEquals("Name", record.get().getValueAsList().get(0).getValueAsString());
-        Assert.assertEquals(10, Long.parseLong(parser.getOffset()));
+        Assert.assertEquals("id::7", record.getHeader().getSourceId());
+        Assert.assertEquals("Newman, Al", record.get().getValueAsList().get(0).getValueAsString());
+        Assert.assertEquals(8, Long.parseLong(parser.getOffset()));
         record = parser.parse();
-        Assert.assertNull(record);
-        Assert.assertEquals(-1, Long.parseLong(parser.getOffset()));
+        Assert.assertNotNull(record);
+        Assert.assertEquals("Salazar, Argenis", record.get().getValueAsList().get(0).getValueAsString());
+        Assert.assertEquals(9, Long.parseLong(parser.getOffset()));
+        System.out.print(record);
+
         parser.close();
 
     }
 
+
+    @Test
+    public void TestSASXPTLastOffset() throws Exception {
+
+        SASXportFileIterator iterator = new SASXportFileIterator(inputStream);
+        DataParser parser = new SASXPTDataParser(iterator, getContext(), "id","532");
+        Assert.assertEquals(532,Long.parseLong(parser.getOffset()));
+        Record record = parser.parse();
+        Assert.assertNotNull(record);
+        Assert.assertEquals("id::532", record.getHeader().getSourceId());
+        Assert.assertEquals("7220.0", record.get().getValueAsList().get(3).getValueAsString());
+        Assert.assertEquals(533, Long.parseLong(parser.getOffset()));
+        record = parser.parse();
+        Assert.assertNotNull(record);
+        System.out.print(record);
+        Assert.assertEquals("96.0", record.get().getValueAsList().get(6).getValueAsString());
+        Assert.assertEquals(534, Long.parseLong(parser.getOffset()));
+        record = parser.parse();
+        Assert.assertEquals(-1, Long.parseLong(parser.getOffset()));
+        record = parser.parse();
+        Assert.assertNull(record);
+        parser.close();
+
+    }
+
+    @Test
+    public void testgetOffset() throws Exception {
+        SASXportFileIterator iterator = new SASXportFileIterator(inputStream);
+        DataParser parser = new SASXPTDataParser(iterator , getContext(),"id" , "43");
+        Assert.assertEquals(43, Long.parseLong(parser.getOffset()));
+    }
+
+
     @Test(expected = IOException.class)
     public void testClose() throws Exception {
-
-        SASXportFileIterator iterator = new SASXportFileIterator("/Users/chitranair/Downloads/Test.xpt",0);
+        SASXportFileIterator iterator = new SASXportFileIterator(inputStream);
         DataParser parser = new SASXPTDataParser(iterator, getContext(), "id","0");
         parser.close();
         parser.parse();
     }
-
-
-
 }
