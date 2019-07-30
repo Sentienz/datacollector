0433096a83306ec2764a4454facce29a04ce252d
rifat-khanam <52399179+rifat-khanam@users.noreply.github.com>
2019-07-30 17:23:10 +0530

    Update SASXPTParseFactory.java

--- commonlib/src/main/java/com/streamsets/pipeline/lib/parser/sasxpt/SASXPTParseFactory.java
+++ commonlib/src/main/java/com/streamsets/pipeline/lib/parser/sasxpt/SASXPTParseFactory.java
@@ -1,4 +1,5 @@
 package com.streamsets.pipeline.lib.parser.sasxpt;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
 import java.util.Collections;
@@ -7,7 +8,9 @@ import java.util.Map;
 import java.util.Set;
 import com.sentienz.sas.xpt.SASXportFileIterator;
 import com.streamsets.pipeline.lib.parser.DataParser;
+import com.streamsets.pipeline.lib.parser.DataParserException;
 import com.streamsets.pipeline.lib.parser.DataParserFactory;
+import com.streamsets.pipeline.lib.parser.Errors;
 
 public class SASXPTParseFactory extends DataParserFactory {
 
@@ -21,14 +24,16 @@ public class SASXPTParseFactory extends DataParserFactory {
 	}
 	
 	@Override
-	public DataParser getParser(String id, InputStream is, String offset) {
+	public DataParser getParser(String id, InputStream is, String offset)  throws DataParserException {
 		SASXportFileIterator sasXportFileIterator = null;
-		try {
-			sasXportFileIterator = new SASXportFileIterator(is);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return new SASXPTDataParser(sasXportFileIterator, getSettings().getContext(), id, offset);
+		
+			try {
+				sasXportFileIterator = new SASXportFileIterator(is);
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+			return new SASXPTDataParser(sasXportFileIterator, getSettings().getContext(), id, offset);
 	}
 	
 	@Override
