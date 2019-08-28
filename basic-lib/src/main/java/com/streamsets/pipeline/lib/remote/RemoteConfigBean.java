7a766eee7835e690345a898b2fcb9f2ad048008a
Robert Kanter <rkanter@streamsets.com>
2019-08-28 14:17:40 -0700

    SDC-12336. SFTP and FTP client should provide timeout property
    Change-Id: I4f37205e4a91c1350893e9ef6b8c90c811eb68ac
    Reviewed-on: https://review.streamsets.net/c/25494
    Reviewed-by: Jarcec Cecho <jarcec@streamsets.com>

--- basic-lib/src/main/java/com/streamsets/pipeline/lib/remote/RemoteConfigBean.java
+++ basic-lib/src/main/java/com/streamsets/pipeline/lib/remote/RemoteConfigBean.java
@@ -163,4 +163,160 @@ public class RemoteConfigBean {
       triggeredByValue = "true"
   )
   public String knownHosts;
+
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.BOOLEAN,
+      defaultValue = "false",
+      label = "Use Client Certificate for FTPS",
+      description = "Enable this if the FTPS Server requires mutual authentication. The client will need to provide " +
+          "a keystore file containing the client certificate.",
+      displayPosition = 70,
+      group = "#1"
+  )
+  public boolean useFTPSClientCert;
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.STRING,
+      label = "FTPS Client Certificate Keystore File",
+      description = "Full path to the keystore file containing the client certificate",
+      displayPosition = 71,
+      group = "#1",
+      dependsOn = "useFTPSClientCert",
+      triggeredByValue = "true"
+  )
+  public String ftpsClientCertKeystoreFile;
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.MODEL,
+      defaultValue = "JKS",
+      label = "FTPS Client Certificate Keystore Type",
+      description = "The FTPS Client Certificate Keystore type",
+      displayPosition = 72,
+      group = "#1",
+      dependsOn = "useFTPSClientCert",
+      triggeredByValue = "true"
+  )
+  @ValueChooserModel(KeyStoreTypeChooserValues.class)
+  public KeyStoreType ftpsClientCertKeystoreType = KeyStoreType.JKS;
+
+  @ConfigDef(
+      required = false,
+      type = ConfigDef.Type.CREDENTIAL,
+      label = "FTPS Client Certificate Keystore Password",
+      description = "The password to the FTPS Client Certificate Keystore File, if applicable.  " +
+          "Using a password is highly recommended for security reasons.",
+      displayPosition = 73,
+      group = "#1",
+      dependsOn = "useFTPSClientCert",
+      triggeredByValue = "true"
+  )
+  public CredentialValue ftpsClientCertKeystorePassword = () -> "";
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.MODEL,
+      defaultValue = "ALLOW_ALL",
+      label = "FTPS Truststore Provider",
+      description = "Providing a Truststore allows the client to verify the FTPS Server's certificate. " +
+          "\"Allow All\" will allow any certificate, skipping validation. " +
+          "\"File\" will allow providing a truststore file containing the certificate. " +
+          "\"JVM Default\" will use the JVM's default truststore.",
+      displayPosition = 80,
+      group = "#1"
+  )
+  @ValueChooserModel(FTPSTrustStoreChooserValues.class)
+  public FTPSTrustStore ftpsTrustStoreProvider = FTPSTrustStore.ALLOW_ALL;
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.STRING,
+      label = "FTPS Truststore File",
+      description = "Full path to the truststore file containing the server certificate",
+      displayPosition = 81,
+      group = "#1",
+      dependsOn = "ftpsTrustStoreProvider",
+      triggeredByValue = "FILE"
+  )
+  public String ftpsTruststoreFile;
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.MODEL,
+      defaultValue = "JKS",
+      label = "FTPS Truststore Type",
+      description = "The FTPS Truststore type",
+      displayPosition = 82,
+      group = "#1",
+      dependsOn = "ftpsTrustStoreProvider",
+      triggeredByValue = "FILE"
+  )
+  @ValueChooserModel(KeyStoreTypeChooserValues.class)
+  public KeyStoreType ftpsTruststoreType = KeyStoreType.JKS;
+
+  @ConfigDef(
+      required = false,
+      type = ConfigDef.Type.CREDENTIAL,
+      label = "FTPS Truststore Password",
+      description = "The password to the FTPS Truststore file, if applicable.  " +
+          "Using a password is highly recommended for security reasons.",
+      displayPosition = 83,
+      group = "#1",
+      dependsOn = "ftpsTrustStoreProvider",
+      triggeredByValue = "FILE"
+  )
+  public CredentialValue ftpsTruststorePassword = () -> "";
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.BOOLEAN,
+      defaultValue = "false",
+      label = "Disable Read Ahead Stream",
+      description = "If checked, disable the read-ahead streaming functionality of the SSH client.  Disable if" +
+          " experiencing problems with larger files (ex: in whole file).  Note that this will also result in" +
+          " significantly reducing performance.",
+      displayPosition = 100,
+      group = "#0"
+  )
+  public boolean disableReadAheadStream;
+
+  @ConfigDef(
+      required = false,
+      type = ConfigDef.Type.NUMBER,
+      defaultValue = "0",
+      label = "Socket Timeout",
+      description = "The socket timeout in seconds. A value of 0 indicates no timeout.",
+      displayPosition = 110,
+      group = "#0",
+      min = 0
+  )
+  public int socketTimeout = 0;
+
+  @ConfigDef(
+      required = false,
+      type = ConfigDef.Type.NUMBER,
+      defaultValue = "0",
+      label = "Connection Timeout",
+      description = "The connection timeout in seconds. A value of 0 indicates no timeout.",
+      displayPosition = 111,
+      group = "#0",
+      min = 0
+  )
+  public int connectionTimeout = 0;
+
+  @ConfigDef(
+      required = false,
+      type = ConfigDef.Type.NUMBER,
+      defaultValue = "0",
+      label = "Data Timeout",
+      description = "The data timeout in seconds. A value of 0 indicates no timeout.",
+      displayPosition = 112,
+      group = "#0",
+      min = 0
+  )
+  public int dataTimeout = 0;
+
 }
